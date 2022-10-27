42717912afc77d68c973bfd0b82379ece80beefa
wbingc <wenbing.chuan@gmail.com>
2022-10-27 17:56:12 +0800

    added reference

--- src/main/java/com/example/Gateway/GatewayApplication.java
+++ src/main/java/com/example/Gateway/GatewayApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+//https://www.baeldung.com/spring-cloud-gateway
+
 @SpringBootApplication
 @EnableEurekaClient
 public class GatewayApplication {
