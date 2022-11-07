3a8fbee55f7c25bf5a442043e67a1cefcaab4a19
wbingc <wenbing.chuan@gmail.com>
2022-11-07 16:10:18 +0800

    added common lib for logging, pending config

--- src/main/java/com/example/Gateway/GatewayApplication.java
+++ src/main/java/com/example/Gateway/GatewayApplication.java
@@ -3,15 +3,15 @@ package com.example.Gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
 
 //https://www.baeldung.com/spring-cloud-gateway
 
 @SpringBootApplication
 @EnableEurekaClient
+@ComponentScan("com.example.Gateway")
 public class GatewayApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/example/Gateway/filter/PrePostGlobalFilter.java
@@ -0,0 +1,32 @@
+package com.example.Gateway.filter;
+
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+import com.example.Logging.logger.CustomLogger;
+
+@Component
+public class PrePostGlobalFilter implements GlobalFilter, Ordered {
+
+    private final CustomLogger LOGGER = new CustomLogger();
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        //LOGGER.info("Pre-filter: Forwarding API calls to : " + exchange.getRequest().getPath());
+        LOGGER.getLogger().info("Pre-filter: Forwarding API calls to : "  + exchange.getRequest().getPath());
+        return chain.filter(exchange)
+                .then(Mono.fromRunnable( () -> {
+                    //LOGGER.info("Post-filter: Forwarding result back to client.");
+                    LOGGER.getLogger().info("Post-filter: Forwarding result back to client.");
+                }));
+    }
+
+    @Override
+    public int getOrder() {
+        return -1;
+    }
+}
